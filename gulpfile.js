5c58c591041e991edb2e6fff350fcd872ef75e05
agl0809 <agl0809@gmail.com>
2015-12-11 02:27:44 +0100

    close version 0.1.0

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,27 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
-    livereload = require('gulp-livereload'),
     del = require('del');
 
 // Scripts
 gulp.task('scripts', function () {
-    return gulp.src('src/*.js')
-        .pipe(jshint.reporter('default'))
+    return gulp.src('./src/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'))
+        .pipe(notify({
+            title: 'JSHint',
+            message: 'JSHint passed!',
+        }))
         .pipe(concat('person.js'))
         .pipe(gulp.dest('dist/'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest('dist/'))
-        .pipe(notify({message: 'Scripts task complete'}));
+        .pipe(notify({
+            title: "Build",
+            message: 'Build success!'
+        }));
 });
 
 // Clean
@@ -26,20 +34,6 @@ gulp.task('clean', function () {
 });
 
 // Default task
-gulp.task('default', ['clean'], function () {
+gulp.task('build', ['clean'], function () {
     gulp.start('scripts');
-});
-
-// Watch
-gulp.task('watch', function () {
-
-    // Watch .js files
-    gulp.watch('src/*.js', ['scripts']);
-
-    // Create LiveReload server
-    livereload.listen();
-
-    // Watch any files in dist/, reload on change
-    gulp.watch(['dist/**']).on('change', livereload.changed);
-
 });
